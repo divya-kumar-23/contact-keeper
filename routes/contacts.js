a0f22ee0cab2673d3ca0060ac0a09b74439c759d
dk <kumard2312@gmail.com>
2019-08-12 23:53:21 +0530

    vhome

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,21 +1,55 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 //@route GET api/users
 //@desc Get all users contacts
 //@access Private
 
-router.post("/", (req, res) => {
-  res.send("Register a user");
+router.post("/", auth, async (req, res) => {
+  //res.send("Register a user");
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (error) {
+    console.error(err.message);
+
+    res.status(500).send("Server Error");
+  }
 });
 
 //@route POST api/users
 //@desc Add new contacts
 //@access Private
 
-router.post("/", (req, res) => {
-  res.send("add contact");
-});
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("name", "name is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  (req, res) => {
+    //res.send("add contact");
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {name,email,phone,type}=req.body;
+    
+  }
+);
 
 //@route POST api/contacts/:id
 //@desc Update contact
